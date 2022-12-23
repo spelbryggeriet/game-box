a896aa7bd90d31019aa8b60912a2d5cbb20ed527
Hampus Lidin <hampuslidin@gmail.com>
2022-12-23 19:27:37 +0100

    Docker build (#6)
    * Implement Dockerfile
    
    * actions: Implement build step in create release workflow
    
    * code: Get static files dir from environment variable
    
    * actions: Change invalid context variable

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,5 @@
+use std::{borrow::Cow, env};
+
 use rocket::{
     fs::{relative, FileServer},
     launch,
@@ -5,5 +7,8 @@ use rocket::{
 
 #[launch]
 async fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from(relative!("../frontend/dist/")))
+    let static_dir = env::var("GAME_BOX_STATIC_DIR")
+        .map(Cow::Owned)
+        .unwrap_or_else(|_| relative!("../frontend/dist/").into());
+    rocket::build().mount("/", FileServer::from(&*static_dir))
 }
--- frontend/README.md
+++ frontend/README.md
@@ -6,8 +6,8 @@ To set up the environment for development, add a WASM target (note, this assumes
 rustup target add wasm32-unknown-unknown
 ```
 
-To be able to build, run and bundle the application, you will need the Dioxus CLI as well:
+To be able to build, run and test the application, you will need the trunk command as well:
 
 ```bash
-cargo install dioxus-cli
+cargo install trunk
 ```
